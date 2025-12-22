75d5d4d996c8a4c869d9b973e9fe266db027b5f7
SebaCoder <sebastianleiva2001@gmail.com>
2025-12-22 01:56:36 -0300

    Add out and configure project

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',
+   images: {
+    unoptimized: true,
+  },
+  basePath: "/colegio-el-refugio",
+  assetPrefix: "/colegio-el-refugio/",
 };
 
 export default nextConfig;

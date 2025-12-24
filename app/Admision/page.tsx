975ce9e78631c3c04e99146d0ea2f48ca220cfdd
SebaCoder <sebastianleiva2001@gmail.com>
2025-12-23 23:07:56 -0300

    Add button to pdf

--- app/Admision/page.tsx
+++ app/Admision/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import AppImage from "@/components/AppImage";
 import Description from "@/components/Description";
 import Stepper from "@/components/Stepper";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
+import { CursorArrowRaysIcon } from "@heroicons/react/24/outline";
 
 
 export default function Page() {
@@ -17,12 +19,23 @@ export default function Page() {
                         postular de manera transparente y sencilla a nuestro colegio.</p>
                     </div>
                     <div className="w-full">
-                        <Link className="bg-red-600 text-white w-40 rounded py-1 px-3 mx-auto" href={'https://www.sistemadeadmisionescolar.cl/'} target="_blank">Ir a postular</Link>
+                        <Link className="bg-red-600 text-white w-40 rounded py-1 px-3 mx-auto hover:bg-red-500 transition-colors" href={'https://www.sistemadeadmisionescolar.cl/'} target="_blank">Ir a postular</Link>
                     </div>
                 </div>
             </div>
             <div className=" md:px-3 pt-20">
                 <Stepper/>
+            </div>
+            <div className="mt-20 mb-5 px-3">
+                <h2 className="text-black dark:text-white text-center">Conoce nuestro colegio.</h2>
+                <p className="text-center text-black dark:text-white max-w-xl mx-auto mt-3">
+                    Descarga o revisa nuestro documento institucional con información general,
+                    proyecto educativo y aspectos relevantes de nuestra comunidad escolar.
+                </p>
+                <div className="flex justify-center gap-x-5 mt-10">
+                    <Link href={'/pdf/colegio.pdf'} download={true} className="bg-red-600 px-2 py-1 rounded flex flex-row-reverse gap-x-2 w-min"><ArrowDownTrayIcon className="w-6 h-6"/>PDF</Link>
+                    <Link href={'/pdf/colegio.pdf'} target="_blank" className="bg-red-600 px-2 py-1 rounded flex flex-row-reverse gap-x-2 w-min"><CursorArrowRaysIcon className="w-6 h-6"/>PDF</Link>
+                </div>
             </div>
             <div className="w-full px-10">
                 <Description/>
